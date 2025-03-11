53445c8969da774ad9ff154377a2ebf1949b427a
rafaeIf <rafa.fernandes16052007@gmail.com>
2025-03-10 22:56:16 -0300

    primeira aula (atualizado)

---
+++ java-02/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        int idade = 27;
+        double salario = 2101.50;
+        char sexo = 'm';
+        boolean casado = true;
+        String nome = "Moisés";
+
+            //sout + tab = praticidade
+        System.out.println(idade);
+        System.out.println(salario*1.1);
+        System.out.println(sexo);
+        System.out.println(casado);
+        System.out.println(nome);
+
+    }
+}
---
+++ java-03_atividade/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int caminhao=200000;
+        int bitrem=400000;
+        int furgao=100000;
+        int vuc=100000;
+
+        int soma = (caminhao + bitrem + furgao + vuc);
+        System.out.println(soma);
+    }
+}
